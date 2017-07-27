af30e8526ba8beb5f605970fdb7b0edf5b9f7042
Sean Olson <olson.sean.k@gmail.com>
2017-07-27 12:16:18 -0700

    Update `generate` module documentation.

--- src/generate/mod.rs
+++ src/generate/mod.rs
@@ -1,30 +1,39 @@
-//! This module provides tools for generating meshes for simple shapes like
-//! cubes and spheres.
+//! Mesh generation from primitives like cubes and spheres.
 //!
-//! The interface is iterator-based and begins with a unit shape and
-//! manipulates its constituent primitives like points, lines, and polygons.
-//! All shapes provide position information and some can additionally generate
-//! index, texture, and conjoint point information as well.
+//! This module provides unit primitives that can be used to form complex
+//! iterator expressions to generate meshes via a stream of topology and
+//! geometry. This data can be collected into simple buffers for rendering or a
+//! graph (half-edge) for further manipulation.
+//!
+//! Iterator expressions begin with a unit primitive and manipulate its
+//! components like vertices, lines, and polygons. Generation and decomposition
+//! operations are exposed via traits.
 //!
 //! # Examples
 //!
-//! Generating position and index data for a scaled sphere mesh:
+//! Generating position and index buffers for a scaled sphere:
 //!
 //! ```
-//! use plexus::generate::{IndexedPolygons, Points, SpatialPoints, Triangulate};
+//! use plexus::generate::{IndexedPolygons, SpatialVertices, Triangulate, Vertices};
 //! use plexus::generate::sphere::UVSphere;
 //!
-//! let sphere = UVSphere::with_unit_radius(16, 16);
+//! let sphere = UVSphere::<f32>::with_unit_radius(16, 16);
 //! let positions: Vec<_> = sphere
-//!     .spatial_points() // Get the unique set of points.
-//!     .map(|point| point * 10.0) // Scale the points by 10.
+//!     .spatial_vertices() // Generate the unique set of positional vertices.
+//!     .map(|(x, y, z)| (x * 10.0, y * 10.0, z * 10.0)) // Scale the positions by 10.
 //!     .collect();
 //! let indeces: Vec<_> = sphere
-//!     .indexed_polygons() // Get indeces into the unique set of points as polygons.
+//!     .indexed_polygons() // Generate polygons indexing the unique set of vertices.
 //!     .triangulate() // Decompose the polygons into triangles.
-//!     .points() // Decompose the triangles into points (indeces).
+//!     .vertices() // Decompose the triangles into vertices (indeces).
 //!     .collect();
 //! ```
+
+// TODO: Primitives are parameterized over the type of scalars used for spatial
+//       data. This can be interpreted as the vector space. Other data, like
+//       texture coordinates, are not parameterized. It may be more consistent
+//       to parameterize all of this data and to do so on the generator
+//       functions, if possible. For example, `cube.spatial_vertices::<f32>()`.
 
 pub mod cube;
 mod decompose;
