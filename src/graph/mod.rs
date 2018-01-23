9dfa45d0964ba300f7cefaa8142b3dff7db8ae4c
Sean Olson <olson.sean.k@gmail.com>
2018-01-22 17:31:09 -0800

    Update `graph` module documentation.

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,47 +1,59 @@
 //! Half-edge graph representation of meshes.
 //!
 //! This module provides a flexible representation of meshes as a [half-edge
-//! graph](https://en.wikipedia.org/wiki/doubly_connected_edge_list) exposed as
-//! the `Mesh` type. Meshes can store arbitrary geometric data associated with
-//! any topological structure, including vertices, half-edges, and faces.
-//!
-//! These structures can be difficult to construct from individual components;
-//! the `generate` module can be used to produce primitive meshes that can be
-//! converted into a graph.
+//! graph](https://en.wikipedia.org/wiki/doubly_connected_edge_list). Meshes
+//! can store arbitrary geometric data associated with any topological
+//! structure, including vertices, half-edges, and faces.
 //!
 //! # Representation
 //!
-//! Meshes store topological data using keyed storage. That is, structures
-//! representing vertices, edges, and faces, are stored using associative
-//! collections. Keys are exposed as strongly typed and opaque values, which
-//! can be used to refer to a topological structure, e.g., `VertexKey`.
-//! Importantly, raw references are not used, which eases the construction and
-//! manipulation of the graph in both user and library code by avoiding certain
-//! borrowing rules.
+//! `Mesh`es store topological data using associative collections. Keys are
+//! exposed as strongly typed and opaque values, which can be used to refer to
+//! a topological structure, e.g., `VertexKey`. Topology is typically
+//! manipulated using a view.
+//!
+//! A `Mesh` is conceptually composed of vertices, half-edges, and faces.
+//! Half-edges are directed and join vertices. In a consistent mesh, every
+//! half-edge is paired with an opposite half-edge with the opposite direction.
+//! Given a half-edge that connects a vertex `a` to a vertex `b`, that
+//! half-edge will have an opposite half-edge from `b` to `a`. Together, these
+//! half-edges form a composite edge. When the term "edge" is used alone, it
+//! generally refers to a half-edge.
+//!
+//! Half-edges are connected to their interior neighbors (a "next" and
+//! "previous" half-edge). When a face is present in the region formed by a
+//! perimeter of vertices and their half-edges, the half-edges will refer to
+//! that face and the face will refer to one of the half-edges in the circuit.
+//!
+//! Together with vertices and faces, the connectivity of half-edges allows for
+//! effecient traversal of topology. For example, it becomes trivial to find
+//! neighboring topologies, such as the faces that share a given vertex or the
+//! neighboring faces of a given face.
 //!
 //! # Topological Views
 //!
 //! Meshes expose views over their topological structures (vertices, edges, and
-//! faces). Views are accessed via keys or iteration and behave like
-//! references, and are exposed as `...Ref`, `...Mut`, and `Orphan...Mut` types
-//! (immutable, mutable, and orphan views, respectively) summarized below:
+//! faces). Views are accessed via keys or iteration and behave similarly to
+//! references. They provide the primary API for interacting with a mesh's
+//! topology and geometry. There are three types summarized below:
 //!
-//! | Type      | Name           | Traversal | Arity | Geometry  | Topology  |
-//! |-----------|----------------|-----------|-------|-----------|-----------|
-//! | Immutable | `...Ref`       | Yes       | Many  | Immutable | Immutable |
-//! | Mutable   | `...Mut`       | Neighbors | One   | Mutable   | Mutable   |
-//! | Orphan    | `Orphan...Mut` | No        | Many  | Mutable   | N/A       |
+//! | Type      | Name           | Traversal | Exclusive | Geometry  | Topology  |
+//! |-----------|----------------|-----------|-----------|-----------|-----------|
+//! | Immutable | `...Ref`       | Yes       | No        | Immutable | Immutable |
+//! | Mutable   | `...Mut`       | Yes       | Yes       | Mutable   | Mutable   |
+//! | Orphan    | `Orphan...Mut` | No        | No        | Mutable   | N/A       |
 //!
-//! Note that it is not possible to get mutable views from another mutable view
-//! via a traversal, because a mutation may alter the topology and invalidate
-//! the originating view. This also means that mutable operations will always
-//! consume `self`. In general, an immutable traversal of topology can be used
-//! to collect keys that are later used to query and mutate the target
-//! topology.
+//! Immutable and mutable views are much like references. Immutable views
+//! cannot mutate a mesh in any way and it is possible to obtain multiple such
+//! views at the same time. Mutable views are exclusive, but allow for
+//! mutations.
 //!
-//! All views provide access to their associated geometry. Mutable views, like
-//! `FaceMut`, provide topological operations, like triangulation and
-//! extrusion.
+//! Orphan views are similar to mutable views, but they only have access to the
+//! geometry of a specific topological structure in a mesh. Because they do not
+//! know about other vertices, edges, or faces, an orphan view cannot traverse
+//! the topology of a mesh in any way. These views are most useful for
+//! modifying the geometry of a mesh and, unlike mutable views, multiple orphan
+//! views can be obtained at the same time.
 //!
 //! # Circulators
 //!
@@ -85,6 +97,37 @@
 //!     .collect::<Mesh<Point3<f32>>>();
 //! let key = mesh.faces().nth(0).unwrap().key(); // Get the key of the first face.
 //! mesh.face_mut(key).unwrap().extrude(1.0).unwrap(); // Extrude the face.
+//! # }
+//! ```
+//!
+//! Traversing and circulating over a mesh:
+//!
+//! ```rust
+//! # extern crate nalgebra;
+//! # extern crate plexus;
+//! use nalgebra::Point2;
+//! use plexus::graph::Mesh;
+//! use plexus::prelude::*;
+//!
+//! # fn main() {
+//! let mut mesh = Mesh::<Point2<f32>>::from_raw_buffers(
+//!     vec![0, 1, 2, 3],
+//!     vec![(0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0)],
+//!     4,
+//! ).unwrap();
+//! mesh.triangulate().unwrap();
+//!
+//! // Traverse an edge and use a circulator to get the faces of a nearby vertex.
+//! let key = mesh.edges().nth(0).unwrap().key();
+//! for face in mesh.edge_mut(key)
+//!     .unwrap()
+//!     .into_opposite_edge()
+//!     .into_next_edge()
+//!     .into_destination_vertex()
+//!     .faces_mut() // Circulator yields orphan views.
+//! {
+//!     // ...
+//! }
 //! # }
 //! ```
 
