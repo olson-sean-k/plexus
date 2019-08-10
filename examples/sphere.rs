3560aae2059a0ef34885ee7dd39abb8fd7641821
Sean Olson <olson.sean.k@gmail.com>
2019-08-10 13:14:58 -0700

    Rename the plexus-viewer package to pictor.

--- examples/sphere.rs
+++ examples/sphere.rs
@@ -4,15 +4,15 @@ use plexus::primitive;
 use plexus::primitive::generate::{Normal, Position};
 use plexus::primitive::sphere::UvSphere;
 
-use plexus_viewer::pipeline::Vertex;
-use plexus_viewer::{self, Color4};
+use pictor::pipeline::Vertex;
+use pictor::{self, Color4};
 
 type E3 = Point3<f32>;
 
 fn main() {
     let from = Point3::new(0.0, -4.0, 1.0);
     let to = Point3::origin();
-    plexus_viewer::draw_with(from, to, move || {
+    pictor::draw_with(from, to, move || {
         let sphere = UvSphere::new(32, 32);
         primitive::zip_vertices((
             sphere.polygons::<Position<E3>>().triangulate(),
--- examples/teapot.rs
+++ examples/teapot.rs
@@ -2,17 +2,19 @@ use nalgebra::Point3;
 use plexus::encoding::ply::{FromPly, PositionEncoding};
 use plexus::graph::MeshGraph;
 
-use plexus_viewer::pipeline::Vertex;
-use plexus_viewer::{self, Color4};
+use pictor::pipeline::Vertex;
+use pictor::{self, Color4};
+
+type E3 = Point3<f32>;
 
 fn main() {
     let from = Point3::new(0.0, -10.0, 4.0);
     let to = Point3::new(0.0, 0.0, 1.0);
-    plexus_viewer::draw_with(from, to, move || {
+    pictor::draw_with(from, to, move || {
         // Read PLY data into a graph.
         let ply: &[u8] = include_bytes!("../data/teapot.ply");
-        let encoding = PositionEncoding::<Point3<f32>>::default();
-        let (graph, _) = MeshGraph::<Point3<f32>>::from_ply(encoding, ply).expect("teapot");
+        let encoding = PositionEncoding::<E3>::default();
+        let (graph, _) = MeshGraph::<E3>::from_ply(encoding, ply).expect("teapot");
 
         // Convert the graph into a buffer that can be rendered for viewing.
         graph
