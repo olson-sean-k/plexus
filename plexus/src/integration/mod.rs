75ec603c0fb6b1ecd9e82520f414c79bf3bac12f
Sean Olson <olson.sean.k@gmail.com>
2020-04-25 16:56:44 -0700

    Document the `integration` module.

--- plexus/src/integration/mod.rs
+++ plexus/src/integration/mod.rs
@@ -1,3 +1,11 @@
+//! Integration of external crates and foreign types.
+//!
+//! This module provides implementations of traits in Plexus for foreign types.
+//! Integrated crates are re-exported within a sub-module, which can be used to
+//! avoid versioning conflicts.
+//!
+//! Re-exported types are hidden in the documention for Plexus.
+
 pub mod theon {
     #[doc(hidden)]
     pub use ::theon::*;
