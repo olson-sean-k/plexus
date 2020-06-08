d4e9b321e03d265b64ac1094720d225c59d9b1fd
Sean Olson <olson.sean.k@gmail.com>
2020-06-08 11:18:05 -0700

    Do not insert edges before faces when extruding paths.

--- plexus/src/graph/mutation/path.rs
+++ plexus/src/graph/mutation/path.rs
@@ -1,4 +1,3 @@
-use itertools::Itertools;
 use smallvec::SmallVec;
 
 use crate::entity::borrow::Reborrow;
@@ -7,7 +6,6 @@ use crate::entity::view::Bind;
 use crate::graph::edge::Arc;
 use crate::graph::face::{Face, FaceKey};
 use crate::graph::geometry::{Geometric, Geometry, GraphGeometry};
-use crate::graph::mutation::edge;
 use crate::graph::mutation::face::{self, FaceInsertCache};
 use crate::graph::mutation::vertex;
 use crate::graph::mutation::{Consistent, Mutable, Mutation};
@@ -52,40 +50,19 @@ where
     M: Mutable,
     F: Fn(<Geometry<M> as GraphGeometry>::Vertex) -> <Geometry<M> as GraphGeometry>::Vertex,
 {
-    fn extrude_and_insert<M, N, F>(
-        mut mutation: N,
-        a: VertexKey,
-        f: F,
-    ) -> Result<VertexKey, GraphError>
-    where
-        N: AsMut<Mutation<M>>,
-        M: Mutable,
-        F: Fn(<Geometry<M> as GraphGeometry>::Vertex) -> <Geometry<M> as GraphGeometry>::Vertex,
-    {
-        let geometry = VertexView::bind(mutation.as_mut(), a)
-            .ok_or_else(|| GraphError::TopologyNotFound)?
-            .geometry;
-        Ok(vertex::insert(mutation.as_mut(), f(geometry)))
-    }
-
     let PathExtrudeCache { sources } = cache;
-    let mut destinations = SmallVec::<[_; 2]>::with_capacity(sources.len());
-    for (a, b) in sources.iter().cloned().rev().tuple_windows() {
-        let a = extrude_and_insert(mutation.as_mut(), a, &f)?;
-        let b = extrude_and_insert(mutation.as_mut(), b, &f)?;
-        let (_, (ab, _)) = edge::get_or_insert_with(mutation.as_mut(), (a, b), Default::default)?;
-        destinations.push(ab);
-    }
-    let cache = FaceInsertCache::from_storage(
-        mutation.as_mut(),
-        sources.into_iter().chain(
-            destinations
-                .as_slice()
-                .get(0)
-                .map(|ab| ab.into_source())
-                .into_iter()
-                .chain(destinations.into_iter().map(|ab| ab.into_destination())),
-        ),
-    )?;
+    let destinations: SmallVec<[_; 2]> = sources
+        .iter()
+        .cloned()
+        .rev()
+        .map(|source| -> Result<_, GraphError> {
+            let geometry = VertexView::bind(mutation.as_mut(), source)
+                .ok_or_else(|| GraphError::TopologyNotFound)?
+                .geometry;
+            Ok(vertex::insert(mutation.as_mut(), f(geometry)))
+        })
+        .collect::<Result<_, _>>()?;
+    let cache =
+        FaceInsertCache::from_storage(mutation.as_mut(), sources.into_iter().chain(destinations))?;
     face::insert_with(mutation.as_mut(), cache, Default::default)
 }
